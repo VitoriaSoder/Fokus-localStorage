175bf1bdc386bea5b94e2117faa567baddcd0d50
jovispereira <pereirajovis@gmail.com>
2023-08-24 10:55:52 -0300

    Vídeo 4.1 - Desafio

--- script-crud.js
+++ script-crud.js
@@ -73,7 +73,11 @@ function createTask(tarefa) {
 
     const button = document.createElement('button')
 
-    button.classList.add('app__button-edit')
+    button.classList.add('app_button-edit')
+    const editIcon = document.createElement('img')
+    editIcon.setAttribute('src', '/imagens/edit.png')
+
+    button.appendChild(editIcon)
 
     li.onclick = () => {
         selecionaTarefa(tarefa, li)
